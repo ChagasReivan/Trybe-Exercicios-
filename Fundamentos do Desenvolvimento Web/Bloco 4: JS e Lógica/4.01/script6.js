607f50afdd02ba5e081e12e34a04bd817731779c
Reivan Chagas <chagasreivan@gmail.com>
2022-08-19 14:42:53 -0300

    Adiciona mais exercicios

---
+++ Fundamentos do Desenvolvimento Web/Bloco 4: JS e Lógica/4.01/script6.js
@@ -0,0 +1,68 @@
+// let a = 15;
+// let b = 15;
+
+// // console.log(a+b);
+// // console.log(a-b);
+// // console.log(a*b);
+// // console.log(a/b);
+// // console.log(a%b);
+
+// const a = 20
+// const b = 5
+// const c = 33
+
+// if (a>b){
+//     console.log(a)
+// }
+// else{
+//     console.log(b)
+
+// if (a>(b,c)){
+//     console.log(a)
+// }
+// else if (b>(a,c)){
+//     console.log(b)
+// }
+// else {
+//     console.log(c)
+// }
+
+// const a = -20
+
+// if (a<0){
+//     console.log('negative')
+// }
+// else if (a>0){
+//     console.log('positive')
+// }
+// else if (a=0){
+//     console.log('zero')
+// }
+
+let a =80;
+let b =70;
+let c =20;
+
+if ((a+b+c)==180){
+    console.log('true')
+}
+else if (a<0 || b<0 || c<0){
+    console.log('ta doido mermao?')
+}
+else if((a+b+c)!==180){
+    console.log('false')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
